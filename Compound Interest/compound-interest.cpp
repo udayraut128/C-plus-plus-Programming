162fb5208a11ab77246f13fbb3a0d1dae2754c26
Uday Raut <126658455+udayraut128@users.noreply.github.com>
2024-02-03 23:03:51 +0530

    Update compound-interest.cpp

--- Compound Interest/compound-interest.cpp
+++ Compound Interest/compound-interest.cpp
@@ -1,6 +1,22 @@
-#include<iostream>
-
-int main(){
-cout<<"hello";
-return 0;
+  #include <iostream>
+ #include <cmath>
+using namespace std;
+int main() {
+   float p, t, r, a, b, c, d;
+    cout<<"Enter your Principal=";
+   cin>>p;
+   cout<<"Enter your Time=";
+   cin>>t;
+   cout<<"Enter your Rate in RS=";
+   cin>>r;
+   
+   r=r*12;
+   a=1+r/100;
+   b=pow(a,t);
+   
+   c=(b-1)*p;
+   d=p+c;
+   cout<<"Compount Interest="<<c<<endl;
+   cout<<"Total Amount="<<d;
+   return 0;
 }
