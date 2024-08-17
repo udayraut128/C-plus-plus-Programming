5cdf78fd2c72a29bbd77ecae1251468c046f6bbf
coderroot2 <udayraut128@gmail.com>
2024-08-17 22:02:43 +0530

    add function

---
+++ CPP_Tutorials/07_Functions/01_No_parameter_and_Return.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void greet()
+{
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    // Function call
+    greet();
+    return 0;
+}
