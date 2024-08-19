a91aa8a914de7b4c9ad28175d2665c509285b199
coderroot2 <udayraut128@gmail.com>
2024-08-19 22:37:30 +0530

    add file handling code

---
+++ CPP_Tutorials/13_File Handling/03_Appending/code.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream outFile("example.txt", ios::app); // Open file in append mode
+
+    if (outFile.is_open())
+    {
+        outFile << "Appending a new line to the file." << endl; // Append data
+        outFile.close();                                        // Close the file
+        cout << "Data appended to file successfully." << endl;
+    }
+    else
+    {
+        cout << "Unable to open file for appending." << endl;
+    }
+
+    return 0;
+}
---
+++ CPP_Tutorials/13_File Handling/04_Reading_and_Writing/code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    fstream file("data.txt", ios::in | ios::out | ios::trunc); // Open file for reading and writing
+
+    if (file.is_open())
+    {
+        // Writing to the file
+        file << "C++ File Handling Example" << endl;
+        file << "Writing and reading from the same file." << endl;
+
+        // Reset file position to the beginning
+        file.seekg(0, ios::beg);
+
+        // Reading from the file
+        string line;
+        while (getline(file, line))
+        {
+            cout << line << endl; // Display the content
+        }
+
+        file.close(); // Close the file
+    }
+    else
+    {
+        cout << "Unable to open file." << endl;
+    }
+
+    return 0;
+}
